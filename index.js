d81d84aa8b2b95004e8a071844691ba2736759cc
Vladimir Martinenko <teslabest87@gmail.com>
2024-03-05 20:38:11 +0100

    add table

---
+++ index.js
@@ -0,0 +1,575 @@
+let costumers=[
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Floyd Miles',
+    Company : 'Yahoo',
+    Phone_Number: '(205) 555-0100',
+    Email: 'floyd@yahoo.com',
+    Country: 'Kiribati',
+    Status: 'Inactive'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+  {
+    Customer_Name : 'Jane Cooper',
+    Company : 'Microsoft',
+    Phone_Number: '(225) 555-0118',
+    Email: 'jane@microsoft.com',
+    Country: 'United States',
+    Status: 'Active'
+  },
+]
+
+let table = document.getElementById('table');
+let tbody = document.getElementById('tbody');
+
+for (let costumer of costumers) {
+
+	let tr = document.createElement('tr');
+  tr.style= "";
+	
+	let td1 = document.createElement('td');
+	td1.textContent = '';
+	tr.appendChild(td1);
+
+	let td2 = document.createElement('td');
+	td2.textContent = costumer.Customer_Name;
+	tr.appendChild(td2);
+  
+	
+	let td3 = document.createElement('td');
+	td3.textContent = costumer.Company;
+	tr.appendChild(td3);
+
+  let td4 = document.createElement('td');
+	td4.textContent = costumer.Phone_Number;
+	tr.appendChild(td4);
+	
+	let td5 = document.createElement('td');
+	td5.textContent = costumer.Email;
+	tr.appendChild(td5);
+
+	let td6 = document.createElement('td');
+	td6.textContent = costumer.Country;
+	tr.appendChild(td6);
+
+	let td7 = document.createElement('td');
+  let p = document.createElement('p');
+  td7.appendChild(p);
+
+	p.textContent = costumer.Status;
+  if(costumer.Status === 'Active'){
+    p.className = 'activ';
+  }else{
+    p.className = 'inactive';
+  };
+	tr.appendChild(td7);
+
+  let td8 = document.createElement('td');
+	td8.textContent = '';
+	tr.appendChild(td8);
+	
+	tbody.appendChild(tr);
+}
+
+  const content = document.querySelector('.content'); 
+  const itemsPerPage = 7;
+  let currentPage = 0;
+  let prevPage = document.querySelector(".pagination__arrow_prev");
+  let nextPage = document.querySelector(".pagination__arrow_next");
+  let totalPages;
+
+
+function showPage(page) {
+  let data = Array.from(document.querySelectorAll('[style=""]'));
+  console.log(data)
+  let items = data.slice(1); 
+  
+ console.log(items);
+  const startIndex = page * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  items.forEach((item, index) => { 
+    item.classList.toggle('hidden', index < startIndex || index >= endIndex);
+   
+  });
+  updateActiveButtonStates();
+}
+
+function createPageButtons() {
+  let data = Array.from(document.querySelectorAll('[style=""]'));
+  console.log(data)
+  let items = data.slice(1); 
+  console.log(items)
+
+  totalPages = Math.ceil(items.length / itemsPerPage);
+  console.log(totalPages)
+  const paginationContainer =  document.querySelector(".pagination");
+  paginationContainer.querySelectorAll("span").forEach(el => el.remove());
+  for (let i = 0; i < totalPages; i++) {
+
+    const pageButton = document.createElement('span');
+    pageButton.textContent = i + 1;
+    pageButton.addEventListener('click', () => {
+      currentPage = i;
+      console.log(currentPage)
+      showPage(currentPage);
+      
+    });
+      paginationContainer.appendChild(pageButton);
+    }
+    hideOverPages();
+
+  }
+
+
+function updateActiveButtonStates() {
+  const pageButtons = document.querySelectorAll('.pagination span');
+  console.log(pageButtons);
+  pageButtons.forEach((button, index) => {
+    if (index === currentPage) {
+      button.classList.add('active');
+    } else {
+      button.classList.remove('active');
+    }
+    
+  });
+  hideOverPages();
+}
+  createPageButtons();
+  showPage(currentPage);
+  
+  function myFunction() {
+    let input, filter, body, tr, td, i,t, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    body = document.getElementById("tbody");
+    tr = body.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+    for (t = 1; t < 5; t++) {
+  
+      td = tr[i].getElementsByTagName("td")[t];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+  createPageButtons();
+  showPage(currentPage);
+  hideOverPages();
+  }
+  
+
+  function hideOverPages() {
+    const pageButtons = document.querySelectorAll('.pagination span');
+    console.log(pageButtons);
+     let items = [...pageButtons];
+    console.log(items.length);
+    let active;
+    console.log(currentPage);
+    pageButtons.forEach((button, index) => {
+      if (index === currentPage) {
+        active=button;
+        console.log(active);
+    }})
+  console.log(active.previousElementSibling);
+      items.forEach((item) => item.classList.add("_hide","span"));
+      console.log(items);
+      if (items.length >4){
+      for(let i=0;i<4;i++){
+        items[i].classList.remove("_hide");
+      }
+    }else{
+      for(let i=0;i<items.length;i++){
+        items[i].classList.remove("_hide");
+    }
+  }
+      active.classList.remove("_hide");
+      if (active.previousElementSibling) {
+        active.previousElementSibling.classList.remove("_hide");
+      }
+      if (active.nextElementSibling) {
+        active.nextElementSibling.classList.remove("_hide");
+      }
+      items[items.length-1].classList.remove("_hide");
+    }
+  prevPage.addEventListener("click", function() {
+    if (currentPage > 0) {
+      currentPage=currentPage-1
+      showPage(currentPage);
+      console.log(currentPage);
+      updateActiveButtonStates();
+    }
+  });
+  nextPage.addEventListener("click", function() {
+    if (currentPage < totalPages-1) {
+      console.log(totalPages)
+      currentPage=currentPage+1
+      showPage(currentPage);
+      console.log(currentPage);
+      updateActiveButtonStates();
+    }
+  });
+
